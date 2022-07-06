a60c31051165aa7b1c4d8bf432d9801dfeac5523
SLG\ciordanidis <ciordanidis@singularlogic.eu>
2022-07-06 15:35:24 +0300

    finished

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { TimeInterceptor } from './core/time.interceptor';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,7 @@ async function bootstrap() {
 		type: VersioningType.URI,
 		defaultVersion: '1'
 	});
+	app.use(cookieParser());
 	await app.listen(3000);
 }
 bootstrap();
